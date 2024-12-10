99c32793df98a38aba63a734b0822a67786edb1e
Vachek-hooves <vachek0602@gmail.com>
2024-12-10 18:26:49 +0200

    update zoom on region click.

--- Screen/TabScreen/TabMapScreen.js
+++ Screen/TabScreen/TabMapScreen.js
@@ -1,27 +1,52 @@
-import {Platform, StyleSheet, View} from 'react-native';
+import {Platform, StyleSheet, View, Animated} from 'react-native';
 import MapView, {PROVIDER_DEFAULT, Polygon} from 'react-native-maps';
 import {poligonRegions} from '../../data/poligon';
-
-const greeceCoordinates = [
-  {latitude: 39.071, longitude: 22.637},
-  {latitude: 38.31, longitude: 21.444},
-  {latitude: 37.644, longitude: 21.12},
-  {latitude: 36.409, longitude: 22.509},
-  {latitude: 35.368, longitude: 23.703},
-];
+import React, {useRef} from 'react';
 
 const TabMapScreen = () => {
+  const mapAnimation = useRef(new Animated.Value(0)).current;
+  const mapRef = useRef(null);
+
+  const onRegionSelect = region => {
+    // Animate to the selected region
+    mapRef.current.animateToRegion(
+      {
+        latitude: region.coordinates[0].latitude,
+        longitude: region.coordinates[0].longitude,
+        latitudeDelta: 0.5,
+        longitudeDelta: 0.5,
+      },
+      2000,
+    );
+
+    // Animate opacity or scale of the selected region
+    Animated.sequence([
+      Animated.timing(mapAnimation, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(mapAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
   return (
     <View style={styles.container}>
       <MapView
-        provider={PROVIDER_DEFAULT} 
+        ref={mapRef}
+        provider={PROVIDER_DEFAULT}
         style={styles.map}
         region={{
-          latitude: 37.1, // Latitude for Athens, Greece
-          longitude: 24.0, // Longitude for Athens, Greece
-          latitudeDelta: 0.5, // Zoom level
-          longitudeDelta: 4, // Zoom level
-        }}>
+          latitude: 37.9838,
+          longitude: 23.7275,
+          latitudeDelta: 5,
+          longitudeDelta: 3,
+        }}
+      >
         {poligonRegions.map((region, index) => (
           <Polygon
             key={index}
@@ -29,12 +54,11 @@ const TabMapScreen = () => {
             fillColor={region.fillColor}
             strokeColor={region.strokeColor}
             strokeWidth={region.strokeWidth}
-            onPress={() => console.log(region)}
+            onPress={() => onRegionSelect(region)}
             tappable={true}
-            zIndex={10}
+            zIndex={index + 1}
           />
         ))}
-        
       </MapView>
     </View>
   );
