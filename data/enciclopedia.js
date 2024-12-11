91e1818eb4cff1931a9bb0ad3589fae0d64dba94
Vachek-hooves <vachek0602@gmail.com>
2024-12-11 14:25:11 +0200

    update images data

--- data/enciclopedia.js
+++ data/enciclopedia.js
@@ -1,5 +1,6 @@
 export const enciclopedia = [
   {
+    image: require('../assets/images/enciclopedia/1.png'),
     id: 'e1',
     title:
       'Mythology of Ancient Greece: Overview of Major Myths, Gods, Heroes, and Legends that Shaped the Worldview of Ancient Greeks',
@@ -7,42 +8,49 @@ export const enciclopedia = [
       'The mythology of Ancient Greece played a crucial role in shaping the culture and worldview of the ancient Greeks. It served as the foundation for literature, art, and philosophy, explaining both natural phenomena and social laws. The pantheon of Greek gods, including Zeus, Athena, Apollo, Hera, and others, was central to the myths, where gods often intervened in human affairs. These gods had human traits, frequently experiencing emotions, ambitions, and even weaknesses, which made them more relatable to humans.Heroes such as Hercules, Theseus, and Achilles embodied courage and the struggle against supernatural forces. Their deeds and battles with monsters or gods conveyed moral lessons about the importance of resilience, bravery, and justice. Myths often contained lessons that emphasized moral principles, such as humility before the gods, devotion, and fairness.Myths also explained the creation of the world, the laws of nature, and the origin of humanity. One of the most famous myths, the myth of the creation of the world, describes the birth of deities from chaos and the order they established in the universe. Mythology helped the Greeks understand nature, social relationships, and even political structures, particularly in Athens and Sparta.These legends remain part of world culture today, influencing literature, cinema, art, and even psychology. The myths of Ancient Greece continue to inspire due to their depth, universality, and important moral lessons.',
   },
   {
+    image: require('../assets/images/enciclopedia/2a.png'),
     id: 'e2',
     title: 'Ancient Greek City-States: Athens, Sparta, and Others',
     content:
       'The ancient Greek city-states (poleis) had different political systems and social structures, but each played an important role in the development of civilization. Athens became the center of democracy, science, and culture, while Sparta was known for its military strength and oligarchic system.Athens developed democratic ideas, where citizens had the right to participate in political life. Under the leadership of Pericles, Athens became a symbol of democracy and a powerful naval and cultural center. The Delian League, created to fight Persia, allowed Athens to solidify its power in the Mediterranean.Sparta, on the other hand, was completely different from Athens. Spartans were famous for their military discipline, strict upbringing, and oligarchic government. The Spartan system was based on military training, with all other aspects of life, including education, subordinated to military service. Spartans had undeniable military superiority, which ensured their victories in numerous battles.In addition to Athens and Sparta, other important city-states include Corinth, Argos, and Megara, each with its own governance and cultural development. Corinth, for example, was an important trading and cultural center, while Argos was known for its ancient history.Each city-state had its strengths and weaknesses, but together they formed a large cultural and political space that contributed to the development of Greek civilization.',
   },
   {
+    image: require('../assets/images/enciclopedia/3a.png'),
     id: 'e3',
     title: 'The Greco-Persian Wars: From Marathon to Plataea',
     content:
       'The Greco-Persian Wars, which took place between 499 and 449 BC, were a series of conflicts between the Greek city-states and the Persian Empire. They were crucial for preserving the independence of the Greek poleis and their cultural identity.The first major clash was the Battle of Marathon in 490 BC, where the Athenians, led by Miltiades, achieved an unexpected victory, stopping the Persians from capturing Athens. This was an important psychological victory that boosted the spirit of the Greeks.In 480 BC, the Persians, led by Xerxes I, made another attempt to conquer Greece. At the Battle of Thermopylae, the Spartans, led by Leonidas, heroically perished while defending a narrow pass from the vast Persian army. Meanwhile, at sea, the Greeks won the Battle of Salamis, where the Athenians, under Themistocles, dealt a significant defeat to the Persian fleet.The final victory over the Persians was secured at the Battle of Plataea in 479 BC, when the united Greek forces decisively defeated the Persians and ensured peace in the region. These wars solidified Greece status as an independent cultural and political center of the Mediterranean world.',
   },
   {
+    image: require('../assets/images/enciclopedia/4.png'),
     id: 'e4',
     title: 'Athenian Democracy: Principles, Reforms, Achievements',
     content:
       'Athenian democracy was a significant stage in the development of political systems. It began to take shape after the reforms of Cleisthenes in 508 BC and was further developed during the reign of Pericles (443–429 BC). Cleisthenes introduced a system that allowed citizens to participate in political life through the ekklesia (the assembly), where important state matters were decided.Pericles, in turn, introduced payment for public officials, enabling even poor citizens to participate in public affairs. This made Athens more inclusive and ensured equal access to power for all citizens. As a result, Athens became a center of culture, science, and art, supported by democracy.A key feature of Athenian democracy was that not only aristocrats but also ordinary citizens could actively influence politics. This period was marked by the construction of magnificent architectural monuments, such as the Parthenon, and the development of theatrical art with playwrights such as Aeschylus, Sophocles, and Euripides.Athenian democracy laid the foundation for modern democratic systems. Its principles, though differing from today due to the limited participation of slaves and women in politics, had a profound influence on the development of political thought in Europe.',
   },
   {
+    image: require('../assets/images/enciclopedia/5a.png'),
     id: 'e5',
     title: 'Alexander the Great: Conquests and Legacy',
     content:
       'Alexander the Great, the son of Philip II, became one of the greatest conquerors in history. His empire stretched from Greece to India, creating one of the largest political entities in the ancient world. Alexander began his campaigns in 334 BC, defeating the Persians at major battles such as Issus and Gaugamela. His brilliant military strategies allowed him to break the Persian Empire, and his conquests of Egypt and Babylon strengthened his rule.However, his conquests went beyond military victories. Alexander actively encouraged cultural exchange, founding new cities, including Alexandria, which became an important cultural center where Greek and Eastern cultures merged. His policies promoted the spread of Hellenistic culture across vast territories from Egypt to India.After Alexander death in 323 BC, his empire fractured into several parts, marking the beginning of the Hellenistic period. Although the empire did not last long, Alexander legacy in culture, science, and politics left a lasting impact on the Mediterranean and the East.',
   },
   {
+    image: require('../assets/images/enciclopedia/6.png'),
     id: 'e6',
     title: 'Ancient Greek Philosophy: Socrates, Plato, Aristotle',
     content:
       'Ancient Greek philosophy laid the foundation for Western intellectual traditions. Socrates, who lived in the 5th century BC, was the first major philosopher who did not write his teachings but held public debates. His method, known as the Socratic dialogue, involved asking questions that encouraged his interlocutors to think and test their beliefs. He believed wisdom lay in recognizing one’s ignorance.Plato, Socrates student, developed the theory of forms, which posited that the physical world is merely a reflection of ideal, unattainable forms. In his dialogues, he described an ideal state where philosophers should rule, creating the so-called Platonic state. Plato founded the first Academy in Athens, an important center for learning.Aristotle, Plato’s student, took a more practical approach to philosophy. He created systems of logic and metaphysics that became the foundation for subsequent research in various scientific fields. Aristotle also studied ethics and politics, examining the role of citizens in society and defining happiness as the ultimate goal of human life. Through their ideas and discoveries, these philosophers profoundly influenced the development of Western culture, politics, and science.',
   },
   {
+    image: require('../assets/images/enciclopedia/7.png'),
     id: 'e7',
     title: 'Greek Culture: Art, Theater, Literature',
     content:
       'Ancient Greek culture left a deep imprint on the history of art, literature, and theater. One of the key achievements was Greek tragedy, which emerged in the mid-5th century BC. In the tragedies of playwrights like Aeschylus, Sophocles, and Euripides, important moral and social issues were explored. For example, Sophocles "Antigone" addresses the conflict between state laws and moral duty.Greek literature reached great heights, particularly through Homer, whose epics, the "Iliad" and the "Odyssey," became the foundation of classical Greek literature. They not only described heroic deeds but also deeply explored human emotions, moral dilemmas, and the relationships between people and gods.Greek artists also advanced architecture, creating monumental temples like the Parthenon, which became a symbol of Athenian democracy. The Doric, Ionic, and Corinthian styles became standards of architectural design in many cultures.Ancient Greek culture had a significant impact on the subsequent development of European civilization, from art to philosophy and science.',
   },
   {
+    image: require('../assets/images/enciclopedia/8.png'),
     id: 'e8',
     title: 'Greek Gods and Religion',
     content:
