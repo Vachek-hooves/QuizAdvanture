6b9ddf0d77bc518c680d41d19306b30b38475380
Vachek-hooves <vachek0602@gmail.com>
2024-12-11 14:32:45 +0200

    working with quiz screen

--- Screen/StackScreen/StackQuizLevelGameScreen.js
+++ Screen/StackScreen/StackQuizLevelGameScreen.js
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import {useAppContext} from '../../store/context';
 
-const StackQuizLevelGameScreen = () => {
+const StackQuizLevelGameScreen = ({route}) => {
+  const {regionId} = route.params;
+  const {quiz} = useAppContext();
+  const QUIZ = quiz.find(q => q.id === regionId);
+  console.log(QUIZ);
+
   return (
     <View>
       <Text>StackQuizLevelGameScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default StackQuizLevelGameScreen
+export default StackQuizLevelGameScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
