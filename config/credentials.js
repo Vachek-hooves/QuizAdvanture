8b2c712c80bf1cec10a445dccefaa61bec112340
Vachek-hooves <vachek0602@gmail.com>
2025-10-10 15:47:16 +0300

    released

---
+++ config/credentials.js
@@ -0,0 +1,31 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'aAEkYn6URit7zesRvYSchS',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - 4ee780147d003b21dabed79e1bfd24d0
+
+// FB app cred
+export const FB_APP_ID = '4111284102420787';
+export const FB_CLIENT_TOKEN = '25aec5fdea3ad67f3f85861266fc6815';
+
+// Compaign
+// https://prime-stream-box.site/Wc9fZIq5
+
+export const INITIAL_URL = `https://prime-stream-box.site/`;
+
+export const URL_IDENTIFAIRE = `Wc9fZIq5`;
+
+// one signal key ex
+export const ONE_SIGNAL_ID = '4351a85b-6632-443d-86bf-cc8f0d194e3f';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - cc380238-f4be-4a7d-8f4e-1846716c63b6
