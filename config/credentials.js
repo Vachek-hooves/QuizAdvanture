3dbb738eeb4ea89ccb8cd059350603370e8ca509
Vachek-hooves <vachek0602@gmail.com>
2025-10-14 10:39:10 +0300

    released

---
+++ config/credentials.js
@@ -0,0 +1,31 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'gWJqZnHtETy7w7PYbwzY5H',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key -3c703c1bd727e5b60f0f7d20cee5ff4c
+
+// FB app cred
+export const FB_APP_ID = '1740516629961957';
+export const FB_CLIENT_TOKEN = '9f3e278fa923e732083611b1538ab607';
+
+// Compaign
+// https://bright-grid-digital.site/DmsYw7Mv
+
+export const INITIAL_URL = `https://bright-grid-digital.site/`;
+
+export const URL_IDENTIFAIRE = `DmsYw7Mv`;
+
+// one signal key ex
+export const ONE_SIGNAL_ID = 'fc0422e0-0c9b-4b0f-8157-ef4d6436d423';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - 785cdd24-752c-4627-89fc-6c545bf3ce3a
